f9101abcdfa33eccc9f6a67c1b5cf8220497d2d7
veronica <veronicabreguta@yahoo.com>
2018-04-17 13:34:08 -0500

    The order in which the program executes

---
+++ src/main/java/oca/chapter/one/Chick.java
@@ -0,0 +1,15 @@
+package oca.chapter.one;
+
+public class Chick {
+    private String name = "Fluffy";
+    { System.out.println("setting field");}
+    public Chick() {
+        name = "Tiny";
+        System.out.println("setting constructor");
+        }
+
+    public static void main(String[] args) {
+        Chick chick = new Chick();
+        System.out.println(chick.name);
+    }
+}
