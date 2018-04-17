a7041e1802c2a358e6e040f3ef9c68638976baea
veronica <veronicabreguta@yahoo.com>
2018-04-17 11:28:14 -0500

    Imports

---
+++ src/main/java/oca/chapter/one/ImportExample.java
@@ -0,0 +1,11 @@
+package oca.chapter.one;
+
+import java.util.*;
+
+public class ImportExample {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
+}
+
