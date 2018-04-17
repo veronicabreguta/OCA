e8404fe10d86a3d7349b6fcf4418ded42396397c
veronica <veronicabreguta@yahoo.com>
2018-04-17 13:03:59 -0500

    Initialize variables

---
+++ src/main/java/oca/chapter/one/Chicken.java
@@ -0,0 +1,10 @@
+package oca.chapter.one;
+
+public class Chicken {
+    public static void main(String[] args) {
+        String name;
+        name = "Duke";
+        System.out.println(name);
+
+    }
+}
