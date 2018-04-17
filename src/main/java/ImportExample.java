6afbf1b7ec0f53517ba31b55f7dcaf51da3a3265
veronica <veronicabreguta@yahoo.com>
2018-04-17 11:00:55 -0500

    Imports

---
+++ src/main/java/ImportExample.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ImportExample {
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
+}
